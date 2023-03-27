3718287c47ccb46a0e794d9cd7646df90a90d9b7
Savely <savely.domnikov@yandex.ru>
2023-03-28 02:06:34 +0300

    Initial commit

---
+++ src/main/java/pro/sky/savely/springboot/savelyhomeworkspringboot/SavelyHomeworkSpringbootApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.savely.springboot.savelyhomeworkspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavelyHomeworkSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavelyHomeworkSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/savely/springboot/savelyhomeworkspringboot/SavelyHomeworkSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.savely.springboot.savelyhomeworkspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavelyHomeworkSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
