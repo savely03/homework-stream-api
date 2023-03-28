5f1e6cf6a6e06c5b0f08a3884b6877980400d07e
Savely <savely.domnikov@yandex.ru>
2023-03-28 20:10:52 +0300

    Homework completed

---
+++ src/main/java/pro/sky/savely/springboot/savelyhomeworkspringboot/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package pro.sky.savely.springboot.savelyhomeworkspringboot.service;
+
+import pro.sky.savely.springboot.savelyhomeworkspringboot.models.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstName, String lastName);
+
+    Employee removeEmployee(String firstName, String lastName);
+
+    Employee findEmployee(String firstName, String lastName);
+
+
+    List<Employee> findAllEmployees();
+}
